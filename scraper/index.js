e9703ca5da85a24668061ed8f83b63a47a58cb0e
comaverick <maverickhalili@gmail.com>
2026-08-16 15:40:15 +0800

    real fix

--- scraper/index.js
+++ scraper/index.js
@@ -10,9 +10,11 @@ const {
 } = require("@supabase/supabase-js");
 
 const supabaseUrl =
+  process.env.SUPABASE_URL ||
   process.env.REACT_APP_SUPABASE_URL;
 
 const supabaseKey =
+  process.env.SUPABASE_KEY ||
   process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 const supabase =
